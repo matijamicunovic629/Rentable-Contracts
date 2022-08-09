8392b1cd4482f9cdbcb5d25c31f34645a5ee93d0
Kasra Naderi <kasra@yuser.co>
2022-08-09 19:53:00 -0400

    add process.env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,21 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      url: "https://rpc.ankr.com/eth_goerli/",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
 
 export default config;
