37a2d8af52ea6e36594ca94eea3d77d51139c183
pleja <pleja918@gmail.com>
2022-08-10 00:54:44 -0400

    deploy Scripts

--- contracts/TestERC20.sol
+++ contracts/TestERC20.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestERC20 is ERC20 {
     constructor(string memory name_, string memory symbol_) ERC20( name_,symbol_) {
+        mint(msg.sender, 100000000000000000000000000)
     }
 
     function mint(address account, uint256 amount) public virtual {
