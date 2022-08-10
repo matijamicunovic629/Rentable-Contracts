32f627e9e17131b41ba19b6fc479f42ac0d032e8
matijamicunovic629 <micunovicmatija629@gmail.com>
2022-08-10 00:58:36 -0400

    modify config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+require('dotenv').config();
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || '';
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || '';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
